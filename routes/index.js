672617b43b37d74a94cd191682e149137c3fe315
Javier Guimoye <javierguimoye@gmail.com>
2018-02-22 19:59:16 -0500

    tokens

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 'use strict'
 
 const express       = require('express')
-const api           = express.Router()
+const auth          = require('../middlewares/auth')
 const productCtrl   = require('../controllers/product')
+const api           = express.Router()
+
 
 api.get('/product',productCtrl.getProducts) //get sin parametros
 api.get('/product/:productId', productCtrl.getProduct)   //get con dos parametros
 api.post('/product', productCtrl.saveProduct) //post recibiendo y enviando datos
 api.put('/product/:productId', productCtrl.updateProduct) //modificar
 api.delete('/product/:productId', productCtrl.deleteProduct) //eliminar
+api.get('/private',auth.isAuth,function(req,res){
+    res.status(200).send({message:'Tienes acceso'})
+})
 
 module.exports = api
